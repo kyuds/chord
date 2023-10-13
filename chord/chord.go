eb8ebd8b77be10edd453b26074cbbfd8f1f2c136
Daniel Shin <kyuseung1016@gmail.com>
2023-10-13 13:46:49 +0900

    change accessibility of internal struct ChordCLI and add comments describing library methods

--- chord/chord.go
+++ chord/chord.go
@@ -4,28 +4,42 @@ import (
 	"fmt"
 )
 
-type ChordCLI struct {
+type chordcli struct {
 	IPAddress string
 }
 
-func Initialize(ip string) ChordCLI {
+// Initializes the chord client for the user.
+// Will panic if the specified IP and PORT is
+// already in use or has some other network
+// error. 
+func Initialize(ip string) chordcli {
 	fmt.Printf("Initialize! (%s)\n", ip)
-	c := ChordCLI{IPAddress: ip}
+	c := chordcli{IPAddress: ip}
 	return c
 }
 
-func (c *ChordCLI) JoinRing(ip string) {
+// Joins an already existing chord ring by
+// attempting to contact an already existing
+// chord node at address "ip". Will panic
+// if such node is not found or there is
+// any other network error. 
+func (c *chordcli) JoinRing(ip string) {
 	fmt.Printf("Join! (%s)\n", ip)
 }
 
-func (c *ChordCLI) Put(key, val string) {
+// Puts a key, value pair into the chord
+// network. The key is hashed via SHA1. 
+func (c *chordcli) Put(key, val string) {
 	fmt.Printf("Put! (%s %s)\n", key, val)
 }
 
-func (c *ChordCLI) Lookup(key string) {
+// Looks up the given key and returns the
+// IP address of the node that has the key. 
+func (c *chordcli) Lookup(key string) {
 	fmt.Printf("Lookup! (%s)\n", key)
 }
 
-func (c *ChordCLI) Exit() {
+// Performs a planned exit of the node. 
+func (c *chordcli) Exit() {
 	fmt.Println("Exit!")
 }
